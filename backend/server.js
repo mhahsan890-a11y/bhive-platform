01907cc6d875c5b937b0b8dbfebe2e6cd531ca3e
IC <mhahsan890@gmail.com>
2026-03-27 14:11:24 -0700

    downgrade express and fix railway

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,16 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-
 const PORT = Number(process.env.PORT) || 5000;
 const HOST = "0.0.0.0";
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Backend is running");
@@ -69,5 +73,5 @@ app.post("/api/login", (req, res) => {
 });
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
